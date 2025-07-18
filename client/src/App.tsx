752c29edff1bab110c88975d9feb28b9127dc358
Sophia Bhoria <sophia.bhoria7@gmail.com>
2025-07-17 21:19:47 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
+
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
@@ -17,9 +19,9 @@ function Router() {
 }
 
 function App() {
-  return (
+  return ( 
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
+      <TooltipProvider> 
         <Toaster />
         <Router />
       </TooltipProvider>
